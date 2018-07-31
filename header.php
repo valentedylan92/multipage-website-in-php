b49aa6c8dce8d881c3b936c3bf8a49a7e117ec08
Dylan Valente <valente.dylan92@gmail.com>
2018-07-31 16:24:26 +0200

    Adding sass + Html Homepage

---
+++ header.php
@@ -0,0 +1,9 @@
+<header class="header">
+  <ul class="header__menu">
+    <li class="header__item"><a href="" class="header__link"><img src="" alt="Logo Lhiving Small" class="header__logo"></a></li>
+    <li class="header__item"><a href="" class="header__link">Organisation</a></li>
+    <li class="header__item"><a href="" class="header__link">Articles</a></li>
+    <li class="header__item"><a href="" class="header__link">Documents</a></li>
+    <li class="header__item"><a href="" class="header__link">Contact</a></li>
+  </ul>
+</header>
---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/print.css">
+  <title>Lhiving</title>
+</head>
+<body>
+
+  <main class="main">
+    <section class="section section--home">
+      <div class="section__container container home">
+        <h1 class="home__title"><img src="assets/img/logo.gif" alt="Lhiving" class="home__logo"></h1>
+        <p class="home__subtitle"></p>
+        <a href="" class="section__cta">Soutenez nous</a>
+      </div>
+    </section>
+    <section class="section section--presentation">
+      <div class="section__container container">
+        <h2 class="section__title">Qui sommes nous ?</h2>
+        <p class="section__text">
+           Lhiving asbl offre de l’aide psychosociale aux personnes (ainsi qu’à leurs familles) atteintes du VIH ou une autre maladie chronique grave, qui se trouvent dans un situation défavorisée. Notre objectif est d’améliorer la santé et le bien-être en général de nos clients afin qu’ils puissent mener une vie assez autonome dans la société et y participer à part entière. Lhiving les aide à trouver un endroit convenable et abordable pour vivre et fait un accompagnement plus ou moins intensif. On cherche toujours à revaloriser l’énergie que les gens portent en eux.
+        </p>
+      </div>
+    </section>
+    <section class="section section--pillier">
+      <div class="section__container container">
+        <h2 class="section__title">Trois pilliers</h2>
+        <p class="section__text">
+          Lhiving offre un accompagnement sur mesure à travers de trois piliers : l’antenne, l’accompagnement ambulatoire et l’accompagnement semi-résidentiel avec 18 logements de transit.
+        </p>
+        <div class="pilliers">
+          <div class="pillier__item col-12 col-md-4">
+            <img src="assets/img/Antenne-pillier.jpg" alt="" class="pilliers__img">
+            <p class="pilliers__text">
+              La fonction de <span class="pilliers__highlight">l’antenne</span> est triple: tout d’abord c’est l’endroit où les nouvelles demandes sont enregistrées et un premier rendez-vous est fixé avec l’un des travailleurs. En outre, c’est un point de contact qui peut donner des réponses à des demandes ponctuelles très concrètes. Et finalement, c’est la porte d’entrée vers les autres piliers de Lhiving. Lorsque la demande d’aide est trop complexe ou trop grand pour pouvoir y répondre au niveau de l’antenne, on peut offrir à la personne une guidance plus intensive avec un accompagnateur fixe, que ça soit ambulatoire ou semi-résidentiel.
+            </p>
+          </div>
+          <div class="pillier__item col-12 col-md-4">
+            <img src="http://placekitten.com/335/225" alt="" class="pilliers__img">
+            <p class="pilliers__text">
+              Un <span class="pilliers__highlight">accompagnement ambulatoire</span> comprend un soutien au niveau psychosocial et administratif. La guidance est développée en partenariat avec et adapté au client et est de nature intégrale. Vivre avec une maladie chronique grave affecte de nombreux aspects de la vie quotidienne. Lhiving accompagne la personne vers d’autres services et fait des visites à domicile.
+            </p>
+          </div>
+          <div class="pillier__item col-12 col-md-4">
+            <img src="http://placekitten.com/335/225" alt="" class="pilliers__img">
+            <p class="pilliers__text">
+            <span class="pilliers__highlight">L’accompagnement en semi-résidentiel</span> est destiné aux personnes ayant un grand besoin d’être accompagné et pour qui un séjour temporaire dans un de nos 18 logements de transit est nécessaire et possible. Cela est toujours lié à une guidance intensive et intégrale, le but étant également de travailler vers une solution de logement permanente pour la personne, de préférence dans un logement social.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section section--box">
+      <div class="container box-info">
+        <div class="box-info__left col-md-6"><img src="http://placekitten.com/500/550" alt="" class="box-info__img"></div>
+        <div class="box-info__right col-md-6">
+          <p class="box-info__text">Pour pouvoir obtenir un rendez-vous à l’antenne, il n’y a pas de limitation géographique, mais pour l’accompagnement ambulatoire où semi-résidentiel, la personne doit résider dans la Région de Bruxelles-Capitale.</p>
+          <p class="box-info__text">Il y a une grande marge de flexibilité entre les différents piliers. La transition de l’antenne vers un accompagnement se passe toujours par une consultation au sein de l’équipe de Lhiving, dans laquelle un accompagnateur fixe est nommé. Une personne peut toujours entrer en guidance pour une période courte ou longue si nécessaire, par exemple suite à un déménagement, une aggravation de la maladie, une agrandissement de famille ou d’autres événements drastiques.</p>
+          <p class="box-info__text">En outre, l’association a également une série d’activités, actuellement réservées au public cible initial. Dans ces activités de groupe hebdomadaires le soutien par les pairs est un élément central.</p>
+        </div>
+      </div>
+    </section>
+    <section class="section section--listing">
+      <div class="section__container container">
+        <h2 class="section__title">Facettes d'un accompagnement dans le travail ambulatoire et semi-résidentiel</h2>
+        <div class="listings">
+          <div class="listings__left col-md-6">
+            <p class="listings__description">Les domaines suivants sont toujours repris dans nos accompagnements: </p>
+            <ul class="listing">
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Le logement</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Le bien-être physique et mental</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Le soutien familial et pédagogique</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>L'administration et la sécurité sociale</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>La gestion budgétaire</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>La participation active dans la société</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Le réseau social</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>La prévention et le respect de la thérapie</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Vivre et vieillir avec une maladie chronique</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Une perspective d'avenir</li>
+            </ul>
+          </div>
+          <div class="listings__right col-md-6">
+            <p class="listings__description">Á travers la guidance, on utilise les méthodiques suivantes :</p>
+            <ul class="listing">
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>L'acommpagnement intégral sur mesure</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Le travail persistant</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Visite à domicile</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Une présence accessible</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Une fonction passerelle</li>
+              <li class="listing__item"><i class="listing__icon fas fa-check"></i>Le soutien par les pairs</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section section--donation">
+      <div class="section__container container">
+        <h2 class="section__title">Soutenez nous !</h2>
+        <p class="section__text">
+           Lhiving asbl offre de l’aide psychosociale aux personnes (ainsi qu’à leurs familles) atteintes du VIH ou une autre maladie chronique grave, qui se trouvent dans un situation défavorisée. Notre objectif est d’améliorer la santé et le bien-être en général de nos clients afin qu’ils puissent mener une vie assez autonome dans la société et y participer à part entière. Lhiving les aide à trouver un endroit convenable et abordable pour vivre et fait un accompagnement plus ou moins intensif. On cherche toujours à revaloriser l’énergie que les gens portent en eux.
+        </p>
+        <a href="" class="section__cta"></a>
+      </div>
+    </section>
+  </main>
+</body>
+</html>
